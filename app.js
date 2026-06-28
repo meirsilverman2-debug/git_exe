8c3a2edf75b28cc8382dd81053d3fb4ec32b9a18
Meir925man <meirsilverman2@gmail.com>
2026-06-28 14:30:44 +0300

    feat/subtract function

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import rl from "readline-sync"
 console.log("Hello world!")
-const  name = rl.question("Please enter your name: ")
+const name = rl.question("Please enter your name: ")
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
