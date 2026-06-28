5a7ec884412b851f88600ec19279a0a6f23c17ef
Meir925man <meirsilverman2@gmail.com>
2026-06-28 15:27:49 +0300

    chore/ add d insted of a

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import rl from "readline-sync"
 console.log("Hello world!")
 const name = rl.question("Please enter your name: ")
 
-function add(a, b) {
-    return a + b;
+function add(d, b) {
+    return d + b;
 }
 
 function subtract(a, b) {
@@ -13,3 +13,6 @@ function subtract(a, b) {
 function multiply(a, b){
     return a * b
 }
+
+console.log("hi nisim")
+console.log("byyyyyyye")
